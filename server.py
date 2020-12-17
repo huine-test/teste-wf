f7715da23b8e912c551998c68ca776ad76795ef4
Gabriel Gisoldo <gabrielgisoldi@gmail.com>
2020-12-17 15:49:35 -0300

    Server teste

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request
 from hmac import HMAC, compare_digest
-from hashlib import sha1
+from hashlib import sha256
 
 app = Flask(__name__)
 
@@ -21,10 +21,11 @@ def verify_signature(req):
     received_sign = req.headers.get(
         'X-Hub-Signature-256').split('sha256=')[-1].strip()
     secret = os.environ.get('GITHUB_SECRET').encode()
-    expected_sign = HMAC(key=secret, msg=req.data, digestmod=sha1).hexdigest()
+    expected_sign = HMAC(
+        key=secret, msg=req.data, digestmod=sha256).hexdigest()
     return compare_digest(received_sign, expected_sign)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
