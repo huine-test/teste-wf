cc2ffbdfec7220b27aeb7a04a52a5ef87598525c
Gabriel Gisoldo <gabrielgisoldi@gmail.com>
2020-12-17 15:38:26 -0300

    Server teste

--- server.py
+++ server.py
@@ -1,18 +1,31 @@
 import os
 from flask import Flask, request
+from hmac import HMAC, compare_digest
+from hashlib import sha1
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=["POST", "GET"])
-def hello_world():
+def webhook():
     """."""
-    json_body = request.get_json()
-    headers = request.headers
-    app.logger.debug({"body": json_body, "headers": headers})
-    return 'Hello, World!'
+    app.logger.debug(os.environ)
+    # if request.method == 'POST':
+    #     if verify_signature(request):
+    #         # make something
+    #         return 'Successfully', 200
+    #     return 'Forbidden', 403
+    # return 'Not allowed', 405
+
+
+def verify_signature(req):
+    received_sign = req.headers.get(
+        'X-Hub-Signature-256').split('sha256=')[-1].strip()
+    secret = 'my_secret_string'.encode()
+    expected_sign = HMAC(key=secret, msg=req.data, digestmod=sha1).hexdigest()
+    return compare_digest(received_sign, expected_sign)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True, )
-
