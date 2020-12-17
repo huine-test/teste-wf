0e013744ac69673efbc57adb302064028a3b23a3
Gabriel Gisoldo <gabrielgisoldi@gmail.com>
2020-12-17 15:18:11 -0300

    Server teste

---
+++ server.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST", "GET"])
+def hello_world():
+    """."""
+    return 'Hello, World!'
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
